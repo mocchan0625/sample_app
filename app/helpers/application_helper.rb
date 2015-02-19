50a62f65da57bb66665c40bceaa6373204eb1d9a
mocchan0625 <yuya.mottate0625@gmail.com>
2015-02-19 23:29:53 +0900

    full_titelヘルパーの追加

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #ページ毎に完全なタイトルを表示
+  def full_title(page_title)
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
