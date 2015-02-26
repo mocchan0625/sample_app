f7582da00ff70fb30b81f9702b4fe4f64b5c298e
mocchan0625 <yuya.mottate0625@gmail.com>
2015-02-26 19:40:37 +0900

    ユーザー新規登録の完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "新規会員登録に成功しました。"
       redirect_to @user
     else
       render 'new'
