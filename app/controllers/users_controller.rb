f7057d8f6d20fd3d5e95bb167f3ee1ccb48d5cd8
mocchan0625 <yuya.mottate0625@gmail.com>
2015-03-01 19:35:49 +0900

    ユーザー一覧に削除機能の追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,27 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
 
   def index
-    @users = User.all
-  end
-
-  def new
-    @user = User.new
+    @users = User.paginate(page: params[:page])
   end
 
   def show
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      sign_in user
-      redirect_back_or user
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
-      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
@@ -29,13 +30,19 @@ class UsersController < ApplicationController
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-      flash[:success] = "プロフィール更新完了"
+      flash[:success] = "Profile updated"
+      sign_in @user
       redirect_to @user
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
 
   private
@@ -44,6 +51,8 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+
+    # Before filters
 
     def signed_in_user
       unless signed_in?
@@ -56,4 +65,8 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
-end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+  end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,7 @@
 module SessionsHelper
 
   def sign_in(user)
-    remember_token = User.new_remember_token
-    cookies.permanent[:remember_token] = remember_token
-    user.update_attribute(:remember_token, User.encrypt(remember_token))
+    cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
   end
 
@@ -11,22 +9,21 @@ module SessionsHelper
     !current_user.nil?
   end
 
-  def sign_out
-    self.current_user = nil
-    cookies.delete(:remember_token)
-  end
-
   def current_user=(user)
     @current_user = user
   end
 
   def current_user
-    remember_token = User.encrypt(cookies[:remember_token])
-    @current_user ||= User.find_by(remember_token: remember_token)
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
   def current_user?(user)
     user == current_user
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 
   def redirect_back_or(default)
